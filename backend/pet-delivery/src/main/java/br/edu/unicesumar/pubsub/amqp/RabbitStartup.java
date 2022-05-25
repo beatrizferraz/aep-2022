8d7f34ad5f4ae36fd3664eed56a3cd71ae25d6be
beatrizferraz <51508920+beatrizferraz@users.noreply.github.com>
2022-05-25 02:59:09 -0300

    codigo base

---
+++ backend/pet-delivery/src/main/java/br/edu/unicesumar/pubsub/amqp/RabbitStartup.java
@@ -0,0 +1,39 @@
+package br.edu.unicesumar.pubsub.amqp;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.amqp.core.AmqpAdmin;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.Binding.DestinationType;
+import org.springframework.amqp.core.FanoutExchange;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class RabbitStartup {
+
+    @Autowired
+    private AmqpAdmin amqpAdmin;
+
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+
+    @PostConstruct
+    private void adiciona() {
+
+        Queue filaTeste = new Queue("fila-teste", true, false, false);
+        FanoutExchange fanoutExchangeTeste = new FanoutExchange("fanout-exchange-teste", true, false);
+        Binding bindingTeste = new Binding(filaTeste.getName(), DestinationType.QUEUE, fanoutExchangeTeste.getName(), "", null);
+        this.amqpAdmin.declareQueue(filaTeste);
+        this.amqpAdmin.declareExchange(fanoutExchangeTeste);
+        this.amqpAdmin.declareBinding(bindingTeste);
+
+        this.rabbitTemplate.convertAndSend(fanoutExchangeTeste.getName(), "", "message");
+
+
+    }
+
+}
