2cd80e7c2946d526e2b3be772e55805f3ca0a633
beatrizferraz <51508920+beatrizferraz@users.noreply.github.com>
2022-06-02 04:21:23 -0300

    Projeto Java

---
+++ codigo/petdelivery/src/main/java/unicesumar/aep/petdelivery/PetdeliveryApplication.java
@@ -0,0 +1,13 @@
+package unicesumar.aep.petdelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetdeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetdeliveryApplication.class, args);
+	}
+
+}
---
+++ codigo/petdelivery/src/test/java/unicesumar/aep/petdelivery/PetdeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package unicesumar.aep.petdelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetdeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
